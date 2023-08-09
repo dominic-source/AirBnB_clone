5caa2416b5b4501b512cf925881decc690ea7f14
Obinna Ukwa <obikelv@gmail.com>
2023-08-09 03:10:50 -0700

    updated test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,8 +8,21 @@ from datetime import datetime
 class TestBase(unittest.TestCase):
     """class of functions to run unittests"""
 
-    def test_save(self):
-        """test the save function"""
-        sv = BaseModel()
-        sv.save()
-        self.assertEqual(sv.updated_at, datetime.now())
+    def setUp(self):
+        """function to initialize BaseModel"""
+        self.base = BaseModel()
+
+    def test_created_at_is_datetime(self):
+        self.assertEqual(datetime, type(self.base.created_at))
+
+    def test_updated_at_is_datetime(self):
+        self.assertEqual(datetime, type(self.base.updated_at))
+
+    def test_unique_id(self):
+        base2 = BaseModel()
+        self.assertNotEqual(self.base.id, base2.id)
+
+    def test_different_created_at(self):
+        base1 = BaseModel()
+        base2 = BaseModel()
+        self.assertLess(base1.created_at, base2.created_at)
