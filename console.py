8b083ee4f66bb46bee2cfceef98f176ca5e49dc3
dominic-source <chinonsodomnic@gmail.com>
2023-08-11 06:00:48 -0700

    remove bugs from the console file

--- console.py
+++ console.py
@@ -22,7 +22,6 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, line):
         """Exit the program when it gets to the End of File"""
-        
         print(line)
         return True
 
@@ -30,84 +29,93 @@ class HBNBCommand(cmd.Cmd):
         """Ensure that when and empty line is entered nothing is executed"""
         pass
 
-    def do_create(self, newModel=None):
+    def do_create(self, newModel):
         """Creates a new instance of a Model and saves it"""
-        if newModel is None:
+
+        newModel = newModel.split()
+        if len(newModel) == 0:
             print('** class name missing **')
-        elif newModel not in HBNBCommand.list_of_models:
+        elif newModel[0] not in HBNBCommand.list_of_models:
             print("** class doesn't exist **")
         else:
-            newModel = BaseModel()
-            newModel.save()
-            print(newModel.id)
+            if newModel[0] == "BaseModel":
+                createModel = BaseModel()
+                createModel.save()
+            print(createModel.id)
 
-    def do_show(self, *args):
+    def do_show(self, args):
         """Print the string representation of and instance"""
 
-        if args[0] is None:
+        args = args.split()
+        if len(args) == 0:
             print('** class name missing **')
-        elif args[1] not in HBNBCommand.list_of_models:
+        elif args[0] not in HBNBCommand.list_of_models:
             print("** class doesn't exist **")
-        elif args[1] is None:
+        elif len(args) == 1:
             print("** instance id missing **")
         else:
             modelid = args[0] + '.' + args[1]
             if modelid not in storage.all():
                 print("** no instance found **")
             else:
-                value = storage.all()[modelid]
-                newInst = BaseModel(value)
+                value = storage.all()
+                newInst = BaseModel(**(value[modelid]))
                 print(newInst)
 
-    def do_destroy(self, nameModel=None, mid=None):
+    def do_destroy(self, args):
         """This will destroy the class given it name and id"""
-
-        if nameModel is None:
+        args = args.split()
+        if len(args) == 0:
             print('** class name missing **')
-        elif nameModel not in HBNBCommand.list_of_models:
+        elif args[0] not in HBNBCommand.list_of_models:
             print("** class doesn't exist **")
-        elif mid is None:
+        elif len(args) == 1:
             print("** instance id missing **")
         else:
-            modelid = ".".join([nameModel, mid])
+            modelid = ".".join([args[0], args[1]])
             if modelid not in storage.all():
                 print("** no instance found **")
             else:
                 del storage.all()[modelid]
                 storage.save()
-                
 
-    def do_all(self, nameModel=None):
+    def do_all(self, nameModel):
         """
         Prints all string representation of all the
         instances based or not on the class name
-        
         """
         printout = []
-
-        if nameModel is None or len(nameModel) == 0:
-            value = storage.all().copy()
+        nameModel = nameModel.split()
+        if len(nameModel) == 0:
+            value = storage.all()
             if len(value) > 0:
                 for key, evalue in value.items():
-                    newInst = BaseModel(evalue)
+                    newInst = BaseModel(**evalue)
                     printout.append(newInst.__str__())
         else:
-            if nameModel not in HBNBCommand.list_of_models:
+            if nameModel[0] not in HBNBCommand.list_of_models:
                 print("** class doesn't exist **")
             else:
-                value = storage.all().copy()
+                value = storage.all()
                 for key, evalue in value.items():
-                    if nameModel == key.split('.')[0]:
-                        newInst = BaseModel(evalue)
+                    if nameModel[0] == key.split('.')[0]:
+                        newInst = BaseModel(**evalue)
                         printout.append(newInst.__str__())
-        
+
         if len(printout) != 0:
             print(printout)
 
-    def do_update(self, *args):
+    @staticmethod
+    def merge(args):
+        """merge strings together"""
+
+
+    def do_update(self, args):
         """Updates an instance base on the class name and id"""
-        
-        if args is None:
+
+        args = args.split()
+        print(args)
+        if len(args) == 0:
             print("** class name missing **")
         elif args[0] not in HBNBCommand.list_of_models:
             print("** class doesn't exist **")
@@ -123,16 +131,21 @@ class HBNBCommand(cmd.Cmd):
         else:
             nameModelId = '.'.join([args[0], args[1]])
             store = storage.all()
-            value = store[nameModelId][args[2]]
             try:
+                value = store[nameModelId][args[2]]
+            except ValueError:
+                pass
+            except KeyError:
+                store[nameModelId][args[2]] = args[3]
+            else:
                 if isinstance(value, str):
                     store[nameModelId][args[2]] = str(args[3])
                 elif isinstance(value, int):
                     store[nameModelId][args[2]] = int(args[3])
                 elif isinstance(value, float):
                     store[nameModelId][args[2]] = float(args[3])
-            except ValueError:
-                pass
+                else:
+                    pass
 
 
 if __name__ == '__main__':
