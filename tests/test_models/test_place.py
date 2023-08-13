0586a9c76efb2fe740c890f484b7dc143cd77684
Obinna Ukwa <obikelv@gmail.com>
2023-08-13 15:06:53 -0700

    update place unittest

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
 """This defines unittest for models/user.py"""
 
+import os
+import models
+import unittest
+from models.place import Place
 
-class TestUser:
+
+class TestPlace(unittest.TestCase):
     """unittest for testing user"""
 
-    def test_user(self):
-        """unittest for self in user"""
-        pass
+    def test_no_args(self):
+        self.assertEqual(Place, type(Place()))
+
+    def test_new_in_objects(self):
+        self.assertIn(Place(), models.storage.all().values())
+
+    def test_id_is_str(self):
+        self.assertEqual(str, type(Place().id))
+
+    def test_places_unique_ids(self):
+        my_pl1 = Place()
+        my_pl2 = Place()
+        self.assertNotEqual(my_pl1.id, my_pl2.id)
+
+    def test_args_not_nused(self):
+        my_pl = Place(None)
+        self.assertNotIn(None, my_pl.__dict__.values())
